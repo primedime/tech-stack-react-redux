7c8c1f4bca831741d11ce72455f4d13e95556eb4
Roy Jossfolk <rjossfolk@gmail.com>
2018-01-03 13:11:42 -0500

    - add style to the ListItem.js component

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -13,7 +13,9 @@ class LibraryList extends Component {
     }
 
     renderRow(library) {
-        return <ListItem library={library} />;
+        return ( 
+            <ListItem library={library} />
+        );
     }
 
     render() {
